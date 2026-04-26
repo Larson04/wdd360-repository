4430bdbe5f37d315c20dc538513500b849779834
Larson04 <lar23016@byui.edu>
2026-04-25 18:47:45 -0600

    finish pokeapi review

--- JavascriptReview/review.js
+++ JavascriptReview/review.js
@@ -1,29 +1,48 @@
 // Not finished, look at solution later
 
-const baseURL = "https://pokeapi.co/api/v2/pokemon/";
-const pokemonList = ["pikachu", "charmander", "squirtle", "bulbasaur"];
-const listEl = document.querySelector(".pokeList");
+const baseURL = "https://pokeapi.co/api/v2/pokemon?limit=40";
+const pokemonList = document.querySelector(".pokemon-list");
+const pokemonDetails = document.querySelector(".pokemon-details");
 
-async function getPokemon(pokemon) {
-    const response = await fetch(baseURL + pokemon);
-    const data = await response.json();
-    return data;
+function pokemonListTemplate(item) {
+    return `<li><button data-url="${item.url}">${item.name}</button></li>`;
 }
 
-async function getPokemonList() {
-    pokemonList.forEach(async (pokemon) => {
-        const newData = await getPokemon(pokemon);
-        console.log("pokemon:", pokemon, newData);
-    })
+function pokemonDetailsTemplate(item) {
+    return `
+    <h2>${item.name}</h2>
+    <p>Height: ${item.height}</p>
+    <p>Weight: ${item.weight}</p>
+    <img src="${item.sprites.front_default}" alt="${item.name}">
+    `;
 }
 
-function jsonToHtml(data) {
-
+function renderPokemonList(pokemon) {
+    const pokemonListHtml = pokemon.map(pokemonListTemplate).join("");
+    pokemonList.insertAdjacentHTML('afterbegin', pokemonListHtml);
 }
 
-function innit() {
-    getPokemonList();
-
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+async function pokeListHandler(event) {
+    const pokemonUrl = event.target.dataset.url;
+    const list = await getData(pokemonUrl);
+    const detailsHtml = pokemonDetailsTemplate(list);
+    pokemonDetails.innerHTML = '';
+    pokemonDetails.insertAdjacentHTML('afterbegin', detailsHtml);
+}
+async function innit() {
+    const data = await getData(baseURL);
+    const pokemon = data.results;
+    renderPokemonList(pokemon);
 }
 
+pokemonList.addEventListener('click', pokeListHandler)
 innit();
