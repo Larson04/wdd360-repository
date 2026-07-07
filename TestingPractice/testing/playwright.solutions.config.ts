682626e5e95b70e53c04f88050326e9c99701f4a
Larson04 <lar23016@byui.edu>
2026-07-07 11:03:28 -0600

    finish testing practice

--- TestingPractice/testing/playwright.solutions.config.ts
+++ TestingPractice/testing/playwright.solutions.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/solutions/integration',
   use: {
-    baseURL: 'http://localhost:4322'
+    baseURL: 'http://localhost:4322/src/pages/'
   },
   webServer: {
     command: 'pnpm dev --host 0.0.0.0 --port 4322',
-    url: 'http://localhost:4322',
+    url: 'http://localhost:4322/src/pages/contact.html',
     reuseExistingServer: true,
     timeout: 120000
   }
